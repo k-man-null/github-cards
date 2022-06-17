1debc0455c7b20fc35b191a516c49a83bf82404e
Konchellah <williamoriginal2019@gmail.com>
2022-06-17 22:54:50 +0300

    Passing data into Cardlist via props \n

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
 import React from "react";
 
+const testData = [
+  { name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook" },
+  { name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu" },
+  { name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook" },
+];
+
+const CardList = (props) => (
+ 
+  <div>
+    {props.profiles.map( px => <Card {...px }/> )}
+  </div>
+)
+
 class Card extends React.Component {
   render() {
+    const profile = this.props;
     return (
       <div className="github-profile">
-        One Github Profile...
+        <img src= {profile.avatar_url}/>
+        <div className="info">
+          <div className="name">{profile.name}</div>
+          <div className="company">{profile.company}</div>
+        </div>
       </div>
     );
+  }
+}
+
+class Form extends React.Component {
+  render() {
+    return (
+      <form action="">
+        <input type="text" placeholder="Github Username" />
+        <button> Add card</button>
+      </form>
+    )
   }
 }
 
@@ -15,7 +44,8 @@ class App extends React.Component {
     return (
       <div>
         <div className="header">{this.props.title}</div>
-        <Card />
+        <Form />
+        <CardList profiles={testData} />
       </div>
     )
   }
